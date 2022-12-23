5356c95aaeec8bc57e697dc564c108b8fd05b7c8
taylorngo <taylorngo3@gmail.com>
2022-12-23 23:59:13 +0000

    Saved progress on Instantiation 2022-12-23 23:59:13 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object myObject = new Object();
+        return myObject;
     }
 }
