3fddf224473dd8f7642efab528748270dc8aae34
taylorngo <taylorngo3@gmail.com>
2022-12-24 00:20:08 +0000

    Saved progress on HardcodedLogin 2022-12-24 00:20:08 +0000

--- HardcodedLogin/src/main/java/HardcodedLogin.java
+++ HardcodedLogin/src/main/java/HardcodedLogin.java
@@ -15,6 +15,14 @@ public class HardcodedLogin {
      * @return true if there is a successful login, and false otherwise.
      */
     public boolean login(String username, String password){
-        return false;
+        if(username == "admin"){
+            if(password == "qwerty")
+                return true;
+        }
+        else if(username == "user"){
+            if(password == "password")
+                return true;
+        }
+        return false;        
     }
 }
