de8bbe0e3ec8964cff637f0df770e6e7fb88b012
taylorngo <taylorngo3@gmail.com>
2023-01-06 14:44:46 +0000

    Saved progress on ArraySum 2023-01-06 14:44:46 +0000

--- ArraySum/src/main/java/ArraySum.java
+++ ArraySum/src/main/java/ArraySum.java
@@ -7,7 +7,11 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
-
-        return 0;
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        
+        }
+        return sum;
     }
 }
