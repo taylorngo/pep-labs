2dd8d5de841a9c9647896fd6d970c2c17f1cc054
taylorngo <taylorngo3@gmail.com>
2023-01-17 17:43:09 +0000

    Saved progress on CreatingEndpoints 2023-01-17 17:43:09 +0000

--- CreatingEndpoints/src/main/java/com/revature/JavalinSingleton.java
+++ CreatingEndpoints/src/main/java/com/revature/JavalinSingleton.java
@@ -16,6 +16,13 @@ public class JavalinSingleton {
         
         //write endpoint here
 
+
+        // HTTP GET REQUEST to http://localhost:9000/hello
+    app.get("/hello", ctx -> {
+        //logic to be executed when this endpoint is hit
+        ctx.result("Hello World");
+    });
+
         return app;
     }
     
