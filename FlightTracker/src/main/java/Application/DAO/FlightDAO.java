d70590ed7dbf7b60d314bc2a6407c2373651ae4e
taylorngo <taylorngo3@gmail.com>
2023-01-17 18:27:20 +0000

    Saved progress on GroupBy 2023-01-17 18:27:20 +0000

--- FlightTracker/src/main/java/Application/DAO/FlightDAO.java
+++ FlightTracker/src/main/java/Application/DAO/FlightDAO.java
@@ -33,7 +33,7 @@ public class FlightDAO {
         List<Flight> flights = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "SELECT * FROM flights;";
 
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             ResultSet rs = preparedStatement.executeQuery();
@@ -66,7 +66,7 @@ public class FlightDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "SELECT * FROM flights WHERE flight_id =" + id + ";";
             
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
