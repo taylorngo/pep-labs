1db8c89be2a86c127fe0bbc909110784861709bf
taylorngo <taylorngo3@gmail.com>
2023-01-25 19:03:41 +0000

    Saved progress on ContainsDuplicates 2023-01-25 19:03:41 +0000

--- ContainsDuplicates/src/main/java/ContainsDuplicates.java
+++ ContainsDuplicates/src/main/java/ContainsDuplicates.java
@@ -1,4 +1,5 @@
-
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +9,14 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+        Set nums_set = new HashSet();
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums_set.contains(nums[i])){
+                return true;
+            }
+            nums_set.add(nums[i]);
+        }
         return false;
     }
 }
