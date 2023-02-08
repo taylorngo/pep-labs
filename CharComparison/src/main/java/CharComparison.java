b5d4471de06d7245d343559eab330c1aaa094acb
taylorngo <taylorngo3@gmail.com>
2023-02-08 20:56:57 +0000

    Saved progress on OtherStringMethods 2023-02-08 20:56:57 +0000

--- CharComparison/src/main/java/CharComparison.java
+++ CharComparison/src/main/java/CharComparison.java
@@ -11,6 +11,11 @@ public class CharComparison {
      * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
      */
     public int compare(char[] a, char[] b){
+        for(int i = 0; i < a.length; i++){
+            for (int j = 0; j < b.length; j++){
+                if(a[i].compareTo())
+            }
+        }
         return 0;
     }
 }
